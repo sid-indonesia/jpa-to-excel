b535861f7ecf7b920c172fd675f0e8552d429f6e
Levi <muhammad.levi@sid-indonesia.org>
2021-09-09 12:11:17 +0700

    Affected Issue(s): #1647683884
    What this commit has achieved:
    1. Modified version to `2.0.0-SNAPSHOT`
    2. Added more functionality in ExcelHelper class, it is for validate
    missing values.

--- src/main/java/org/sidindonesia/jpatoexcel/helper/ExcelHelper.java
+++ src/main/java/org/sidindonesia/jpatoexcel/helper/ExcelHelper.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,10 +15,14 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.sidindonesia.jpatoexcel.exception.ExcelWriteException;
 import org.sidindonesia.jpatoexcel.util.CamelCaseUtil;
 import org.sidindonesia.jpatoexcel.util.ReflectionsUtil;
@@ -57,8 +62,10 @@ public final class ExcelHelper {
 			Map<String, Method> getterMethods = getGetterMethodsByFieldName(fields, entityClass);
 			createHeaderRow(sheet, fields);
 
-			fetchAllRowsFromDatabaseAndFillToExcel(context, entityClass, sheet, fields, getterMethods,
-				jpaRepositoryPackageName);
+			List<?> result = retrieveAllRowsFromDatabase(context, entityClass, jpaRepositoryPackageName, "findAll");
+
+			AtomicInteger rowIdx = new AtomicInteger();
+			result.stream().forEach(entry -> fillContentRows(sheet, fields, rowIdx, entry, getterMethods));
 		});
 	}
 
@@ -83,20 +90,25 @@ public final class ExcelHelper {
 		sheet.createFreezePane(0, 1);
 	}
 
-	private static void fetchAllRowsFromDatabaseAndFillToExcel(ApplicationContext context, Class<?> entityClass,
-		Sheet sheet, Field[] fields, Map<String, Method> getterMethods, String jpaRepositoryPackageName) {
+	private static List<?> retrieveAllRowsFromDatabase(ApplicationContext context, Class<?> entityClass,
+		String jpaRepositoryPackageName, String methodName, Object... args) {
 		Class<?> repositoryClass = ReflectionsUtil.getRepositoryClassOfEntity(entityClass, jpaRepositoryPackageName);
 		try {
 			Object repositoryInstance = context.getBean(repositoryClass);
-			Object invokeResult = repositoryClass.getMethod("findAll").invoke(repositoryInstance);
-			List<?> result = (List<?>) invokeResult;
-
-			AtomicInteger rowIdx = new AtomicInteger();
-			result.stream().forEach(entry -> fillContentRows(sheet, fields, rowIdx, entry, getterMethods));
+			if (args.length > 0) {
+				if (args.length == 2) {
+					return (List<?>) repositoryClass.getMethod(methodName, args[0].getClass(), args[1].getClass())
+						.invoke(repositoryInstance, args);
+				}
+				return (List<?>) repositoryClass.getMethod(methodName, args[0].getClass()).invoke(repositoryInstance,
+					args);
+			} else {
+				return (List<?>) repositoryClass.getMethod(methodName).invoke(repositoryInstance);
+			}
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
 			| SecurityException e) {
-			throw new ExcelWriteException("Failed to invoke method `findAll` of class: " + repositoryClass + "\nCause: "
-				+ e.getCause() + "\nMessage: " + e.getMessage());
+			throw new ExcelWriteException(
+				"Failed to invoke method `" + methodName + "` of class: " + repositoryClass + "\nException: ");
 		}
 	}
 
@@ -109,6 +121,76 @@ public final class ExcelHelper {
 			try {
 				Object invokeGetterResult = getterMethods.get(field.getName()).invoke(entry);
 				cell.setCellValue(invokeGetterResult == null ? null : invokeGetterResult.toString());
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				throw new ExcelWriteException(FAILED_TO_IMPORT_DATA_TO_EXCEL_FILE + e.getCause());
+			}
+		}
+	}
+
+	public static ByteArrayInputStream validateAllColumnsAreNotEmpty(ApplicationContext context,
+		String jpaEntityPackageName, String jpaRepositoryPackageName, LocalDateTime fromDate, LocalDateTime untilDate) {
+
+		try (Workbook workbook = new SXSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+
+			Set<Class<?>> entityClasses = ReflectionsUtil.getAllEntityClasses(jpaEntityPackageName);
+
+			entityClasses.forEach(entityClass -> {
+				Sheet sheet = workbook.createSheet(CamelCaseUtil.camelToSnake(entityClass.getSimpleName()));
+
+				Field[] fields = entityClass.getDeclaredFields();
+				Map<String, Method> getterMethods = getGetterMethodsByFieldName(fields, entityClass);
+				createHeaderRow(sheet, fields);
+				createLastHeaderRowsForValidationReportColumns(sheet, fields.length);
+
+				List<?> result = retrieveAllRowsFromDatabase(context, entityClass, jpaRepositoryPackageName,
+					"findAllByDateCreatedBetween", fromDate, untilDate);
+
+				AtomicInteger rowIdx = new AtomicInteger();
+				CellStyle missingValueCellStyle = createMissingValueCellStyle(workbook);
+				result.stream().forEach(entry -> fillAndValidateContentRows(sheet, fields, rowIdx, entry, getterMethods,
+					missingValueCellStyle));
+			});
+
+			workbook.write(out);
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (IOException e) {
+			throw new ExcelWriteException(FAILED_TO_IMPORT_DATA_TO_EXCEL_FILE + e.getMessage());
+		}
+	}
+
+	private static CellStyle createMissingValueCellStyle(Workbook workbook) {
+		XSSFCellStyle missingValueCellStyle = (XSSFCellStyle) workbook.createCellStyle();
+		missingValueCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+		missingValueCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		return missingValueCellStyle;
+	}
+
+	private static void createLastHeaderRowsForValidationReportColumns(Sheet sheet, int lastHeaderCol) {
+		Row headerRow = sheet.getRow(0);
+		Cell totalMissingValuesHeaderCell = headerRow.createCell(lastHeaderCol++);
+		totalMissingValuesHeaderCell.setCellValue("Total Missing Values");
+		Cell filledValuesPercentage = headerRow.createCell(lastHeaderCol++);
+		filledValuesPercentage.setCellValue("Percentage of non-missing values"); // fields.length - total missng values
+																					// / fields.length * 100
+		Cell validationReportCell = headerRow.createCell(lastHeaderCol);
+		validationReportCell.setCellValue("Validation Report"); // which column value is missing
+	}
+
+	private static void fillAndValidateContentRows(Sheet sheet, Field[] fields, AtomicInteger rowIdx, Object entry,
+		Map<String, Method> getterMethods, CellStyle missingValueCellStyle) {
+		Row contentRow = sheet.createRow(rowIdx.incrementAndGet());
+		int contentCol = 0;
+		for (Field field : fields) {
+			Cell cell = contentRow.createCell(contentCol++);
+			try {
+				Object invokeGetterResult = getterMethods.get(field.getName()).invoke(entry);
+
+				if (invokeGetterResult == null) {
+					cell.setCellStyle(missingValueCellStyle);
+				} else {
+					cell.setCellValue(invokeGetterResult.toString());
+				}
+
 			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 				throw new ExcelWriteException(FAILED_TO_IMPORT_DATA_TO_EXCEL_FILE + e.getCause());
 			}
